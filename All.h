b38edf0f870b8639d33a4ef67d9d818520d12255
JJCUBER <jehjeh1020@gmail.com>
2024-10-06 12:33:54 -0400

    [feat] create global/shared header of types/constants

---
+++ All.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ld = long double;
+using ll = long long;
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vd = vector<ld>;
+using vvd = vector<vd>;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<ld, ld>;
+
+constexpr ld EPS = 1e-9;
+
